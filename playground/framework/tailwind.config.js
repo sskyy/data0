a0a3839a978db7c8991d1b69a675d95f93edd083
sskyy <skyking_H@hotmail.com>
2023-07-11 18:10:10 +0800

    framework: add tailwind

---
+++ playground/framework/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./er.html",
+    "./er.ts",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./component/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
