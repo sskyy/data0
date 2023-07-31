9badb2208149156e92b23e651d923751d68c8c4c
sskyy <skyking_H@hotmail.com>
2023-07-31 11:17:05 +0800

    fix: isAtom

--- src/atom.ts
+++ src/atom.ts
@@ -79,6 +79,8 @@ export function atom(initValue: AtomInitialType, interceptor? : AtomInterceptor<
 
     const handler:Handler = {
         get(target, key) {
+            if (key === ReactiveFlags.IS_ATOM) return true
+
             // TODO 是不是也要像 reactive 一样层层包装才行？？？，不然当把这个值传给 dom 元素的时候，它就已经不能被识别出来，也就不能 reactive 了。
             if (isPlainObject(value)) {
                 trackAtomValue(finalUpdater)
