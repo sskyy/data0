7ca749b2471009592fc0c42512a4b28bcf9a3054
sskyy <skyking_H@hotmail.com>
2024-07-29 22:03:28 +0800

    test: add once scheduler test

--- __tests__/common.spec.ts
+++ __tests__/common.spec.ts
@@ -45,5 +45,27 @@ describe('common utils', () => {
         atom(1)
         expect(onceRunsWithValue).toMatchObject([0, 1, 2, 6])
     })
+
+    test('once with scheduler', async () => {
+        const atom1 = atom<number>(0)
+        const onceRunsWithValue:number[] = []
+        
+        once(() => {
+            onceRunsWithValue.push(atom1())
+            if(atom1() > 5) {
+                return true
+            }
+        }, (rerun) => {
+            setTimeout(() => {
+                rerun()
+            }, 100)
+        })
+
+        expect(onceRunsWithValue).toMatchObject([0])
+        atom1(7)
+        expect(onceRunsWithValue).toMatchObject([0])
+        await new Promise(resolve => setTimeout(resolve, 200))
+        expect(onceRunsWithValue).toMatchObject([0, 7])
+    })
 })
 
